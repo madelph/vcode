32c855f89c24a4593f81dd42329e8c3fae2f017c
zhongnandu <zhongnandu@gmail.com>
2011-11-26 05:15:11 +0000

    This is the merged app that need to be troubleshooted

---
+++ Update/Vcode&Vdata/Classes/ClickyQTMovieView.h
@@ -0,0 +1,18 @@
+//
+//  ClickyQTMovieView.h
+//  MovieViewTest
+//
+//  Created by Joey Hagedorn on 3/3/08.
+//  Copyright 2008 University of Illinois & Joey Hagedorn. All rights reserved.
+//	This software is licensed under a BSD license. Please refer to the included
+//  license file for more details.
+
+#import <Cocoa/Cocoa.h>
+#import <QTKit/QTKit.h>
+
+
+@interface ClickyQTMovieView : QTMovieView {
+
+}
+
+@end
---
+++ Update/Vcode&Vdata/LaunchPanel.h
@@ -0,0 +1,20 @@
+//
+//  LaunchPanel.h
+//  VCode
+//
+//  Created by social on 11/24/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface LaunchPanel : NSObject {
+/*
+@private
+    IBOutlet NSWindow *_secondWindow;
+}
+
+- (IBAction)btnManualSecondWindowLoadClick:(id)sender;*/
+}
+@end
