d5d9c4c5bc3b68ab542a1afc659d71a2a1fd4329
quxcc5215 <quxcc5215@gmail.com>
2012-04-13 05:38:47 +0000

    .

---
+++ Update/Vcode&VData2.0-Build-changed/AppDelegate.h
@@ -0,0 +1,47 @@
+//
+//  AppDelegate.h
+//  VCode
+//
+//  Created by Joey Hagedorn on 3/27/08.
+//  Updated and modified by Joshua Hailpern & Zhongnan Du on 12/7/2011
+//  Copyright 2008 University of Illinois & Joey Hagedorn. All rights reserved.
+//	This software is licensed under a BSD license. Please refer to the included
+//  license file for more details.
+
+#import <Cocoa/Cocoa.h>
+//#import <AppKit/AppKit.h>
+
+@interface AppDelegate : NSObject {
+
+	
+@private
+	NSWindow *_windows;
+    IBOutlet NSWindow *_secondWindow;
+    IBOutlet NSArrayController *recent_list;
+    
+    IBOutlet NSTableView *tableview;
+    
+    NSMutableArray *packagesData;
+    
+    NSMutableArray *myRecentFile;
+    
+    IBOutlet NSTextFieldCell * text;
+
+}
+
+@property (assign) IBOutlet NSWindow *windows;
+@property (nonatomic, retain) NSTableView * tableview;
+@property (nonatomic, retain) NSMutableArray * packagesData;
+@property (nonatomic, retain) NSArrayController * recent_list;
+@property (nonatomic, retain) NSMutableArray * myRecentFile;
+
+
+- (IBAction)btnVData:(id)sender;
+-(IBAction)openHelpPDF:(id)sender;
+
+- (IBAction)open:(id)sender;
+- (IBAction)cancel:(id)sender;
+- (IBAction)openOther:(id)sender;
+
+-(void) setPackagesData:(NSMutableArray *)array;
+@end
